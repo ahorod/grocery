95fc1026925f335de2ea3cb2997640d4bb651988
Anna Horodetska and Eric Schoettle <anna@>
2017-03-27 16:46:14 -0700

    sort split textblock into words, sort array

--- js/scripts.js
+++ js/scripts.js
@@ -49,6 +49,7 @@ $(document).ready(function(){
 
     console.log(threeOrMore);
   });
+
 //   var deck = [];
 //   suits = ["spades", "hearts", "clubs", "diamonds"]
 //   values = ["ace", "2", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"]
@@ -63,4 +64,51 @@ $(document).ready(function(){
 //     }
 //   });
 //   console.log(deck);
+
+
+// Our attempt at exercise counting the unique words
+//   var textBlock = "hello hello world world world hey."
+//   var words = textBlock.split([" "]);
+//   console.log(words);
+//   var alphabetized = words.sort();
+//   var alphabetized = alphabetized.push("filler")
+//   console.log(words);
+//   wordCounts = [];
+//
+// var counter = 1
+//   for (var i = 0; i < alphabetized.length; i++) {
+//     if (i>0) {
+//       if (alphabetized[i] === alphabetized[i-1]) {
+//         counter += 1
+//         var output = alphabetized[i]+ counter
+//         debugger
+//       } else {
+//         var output = alphabetized[i]+ counter
+//         var counter = 1
+//         debugger
+//       }
+//     wordCounts.push(output)
+//     }
+//   }
+//   console.log(wordCounts);
+
+
+
+// Stack overflow's response to the same problem: http://stackoverflow.com/questions/5667888/counting-the-occurrences-of-javascript-array-elements
+//function foo(arr) {
+//     var a = [], b = [], prev;
+//
+//     arr.sort();
+//     for ( var i = 0; i < arr.length; i++ ) {
+//         if ( arr[i] !== prev ) {
+//             a.push(arr[i]);
+//             b.push(1);
+//         } else {
+//             b[b.length-1]++;
+//         }
+//         prev = arr[i];
+//     }
+//
+//     return [a, b];
+// }
 });
